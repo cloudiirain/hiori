e374a4e3a4b89f559c7a5e7dff247997f52e09fe
cloudiirain <muffinzapplepear@gmail.com>
2019-03-15 16:51:38 -0400

    Recursively getting all posts and commands in a thread working.

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 "use strict";
 
-var Hiori = require('./lib/Hiori.js');
+const cheerio = require('cheerio');
+const Hiori = require('./lib/Hiori.js');
 
+// Add your {username} and {password} through command line args
 var bot = new Hiori(process.argv[2], process.argv[3]);
 
+/* Fetch all commands in a thread since a given post
 bot.init(async () => {
-  const screenshot = 'screenshot.png';
-  const cmds = await bot.fetchThreadCommandsSince("666035");
-  await console.log(await cmds);
-  //await bot.page.screenshot({ path: screenshot });
-  //console.log('See screenshot: ' + screenshot);
+  console.log('Event List');
+  const cmds = await bot.fetchThreadCommandsSince('4785847');
+  console.log(cmds);
   bot.close();
 });
+*/
+
+/* Fetch all posts in a thread after a given url
+bot.init(async () => {
+  console.log('Event List');
+  //const cmds = await bot.fetchAllThreadPosts('https://forum.novelupdates.com/threads/games-event-list.6650/page-6');
+  const cmds = await bot.fetchAllThreadPosts('https://forum.novelupdates.com/threads/villains-evil-lair.83398/');
+  console.log(cmds);
+  bot.close();
+});
+*/
 
 /* login snippit
 bot.init(async () => {
